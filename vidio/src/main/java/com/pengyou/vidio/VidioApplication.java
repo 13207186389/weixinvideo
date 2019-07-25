4b8cd3c690ff38bbab56fc3c024bba4be37b77ea
13207186389 <120465745@qq.com>
2019-07-25 22:55:57 +0800

    彭优提交

---
+++ vidio/src/main/java/com/pengyou/vidio/VidioApplication.java
@@ -0,0 +1,13 @@
+package com.pengyou.vidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VidioApplication.class, args);
+    }
+
+}
---
+++ vidio/src/test/java/com/pengyou/vidio/VidioApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pengyou.vidio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VidioApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}

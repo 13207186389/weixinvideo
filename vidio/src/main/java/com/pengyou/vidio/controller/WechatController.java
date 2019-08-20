2a42bd856637f481fee91137f0ddbb9e0a3cd96d
13207186389 <120465745@qq.com>
2019-08-20 22:58:06 +0800

    实体类

--- vidio/src/main/java/com/pengyou/vidio/controller/WechatController.java
+++ vidio/src/main/java/com/pengyou/vidio/controller/WechatController.java
@@ -3,14 +3,15 @@ package com.pengyou.vidio.controller;
 
 import com.pengyou.vidio.config.WeChatConfig;
 import com.pengyou.vidio.domain.JsonData;
+import com.pengyou.vidio.domain.User;
+import com.pengyou.vidio.service.UserService;
+import com.pengyou.vidio.utils.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
@@ -21,8 +22,8 @@ public class WechatController {
     @Autowired
     private WeChatConfig weChatConfig;
 
-    //@Autowired
-    //private UserService userService;
+    @Autowired
+    private UserService userService;
 
     /**
      * 拼装微信扫一扫登录url
@@ -43,11 +44,15 @@ public class WechatController {
 
     @GetMapping("/user/callback")
     public void wechatUserCallback(@RequestParam(value = "code",required = true) String code,
-                                   String state, HttpServletResponse response){
+                                   String state, HttpServletResponse response) throws IOException {
 
-
-        //userService.saveWeChatUser(code);
-
+        User user = userService.saveWeChatUser(code);
+        if(user != null){
+            //生成jwt
+            String token = JwtUtils.geneJsonWebToken(user);
+            // state 当前用户的页面地址，需要拼接 http://  这样才不会站内跳转
+            response.sendRedirect(state+"?token="+token+"&head_img="+user.getHeadImg()+"&name="+URLEncoder.encode(user.getName(),"UTF-8"));
+        }
 
     }
 
--- vidio/src/main/java/com/pengyou/vidio/service/impl/UserServiceImpl.java
+++ vidio/src/main/java/com/pengyou/vidio/service/impl/UserServiceImpl.java
@@ -3,11 +3,14 @@ package com.pengyou.vidio.service.impl;
 
 import com.pengyou.vidio.config.WeChatConfig;
 import com.pengyou.vidio.domain.User;
+import com.pengyou.vidio.mapper.UserMapper;
 import com.pengyou.vidio.service.UserService;
 import com.pengyou.vidio.utils.HttpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Map;
 
 @Service
@@ -17,6 +20,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private WeChatConfig weChatConfig;
+
+    @Autowired(required = false)
+    private UserMapper userMapper;
 
 
 
@@ -44,7 +50,26 @@ public class UserServiceImpl implements UserService {
         String city = (String)baseUserMap.get("city");
         String country = (String)baseUserMap.get("country");
         String headimgurl = (String)baseUserMap.get("headimgurl");
+        StringBuilder sb = new StringBuilder(country).append("||").append(province).append("||").append(city);
+        String finalAddress = sb.toString();
+        try {
+            //解决乱码
+            nickname = new String(nickname.getBytes("ISO-8859-1"), "UTF-8");
+            finalAddress = new String(finalAddress.getBytes("ISO-8859-1"), "UTF-8");
 
-        return null;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        User user = new User();
+        user.setName(nickname);
+        user.setHeadImg(headimgurl);
+        user.setCity(finalAddress);
+        user.setOpenid(openId);
+        user.setSex(sex);
+        user.setCreateTime(new Date());
+        userMapper.save(user);
+        return user;
+
     }
 }
